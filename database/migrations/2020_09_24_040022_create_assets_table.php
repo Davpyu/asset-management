4401e654c057526733110bb455b0dfedee2f0d2a
InitalDevil <suryadavid@ymail.com>
2020-09-24 18:29:16 +0700

    asset migration

---
+++ database/migrations/2020_09_24_040022_create_assets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->integer('qty');
+            $table->string('brand');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assets');
+    }
+}
